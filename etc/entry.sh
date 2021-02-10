c8c090a72314e4fad4f47de63125dee41689153e
Achim <a.schnell@wtschnell.de>
2021-02-10 07:24:42 +0100

    Update entry.sh

--- etc/entry.sh
+++ etc/entry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
 				+force_install_dir "${STEAMAPPDIR}" \
-				+app_update "${STEAMAPPID}" \
+				+app_update "${STEAMAPPID}" validate \
 				+quit
 
 cd "${STEAMAPPDIR}"
