1f98be3b7e9699f6b47bd7ad3755e44aec491403
Achim <a.schnell@wtschnell.de>
2021-02-10 07:58:11 +0100

    Update start_valheim.sh

--- etc/start_valheim.sh
+++ etc/start_valheim.sh
@@ -5,14 +5,13 @@ export SteamAppId=892970
 cd "${STEAMAPPDIR}"
 
 # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
-./valheim_server.x86_64 -name ${SERVER_NAME} -port 2456 -world ${SERVER_WORLD} -password ${SERVER_PASSWORD} -public 1 > server.log &
+#./valheim_server.x86_64 -name ${SERVER_NAME} -port 2456 -world ${SERVER_WORLD} -password ${SERVER_PASSWORD} -public 1 > server.log &
 
-export LD_LIBRARY_PATH=$templdpath
-
-echo "Server started"
-echo ""
-read -p "Press RETURN to stop server"
-echo 1 > server_exit.drp
-
-echo "Server exit signal set"
-echo "You can now close this terminal"
+while true; do
+./valheim_server.x86_64 \
+  -name ${SERVER_NAME} \
+  -port 2456 \
+  -world ${SERVER_WORLD} \
+  -password ${SERVER_PASSWORD} \
+  -public 1
+done
