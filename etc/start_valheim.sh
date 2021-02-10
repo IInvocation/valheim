d2b8020c1dcd04e402cd00716f3baa81fcc55fb7
Achim <a.schnell@wtschnell.de>
2021-02-10 07:49:53 +0100

    Update start_valheim.sh

--- etc/start_valheim.sh
+++ etc/start_valheim.sh
@@ -2,8 +2,10 @@ export templdpath=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
 export SteamAppId=892970
 
+cd "${STEAMAPPDIR}"
+
 # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
-./valheim_server.x86_64 -name ${SERVER_NAME} -port 2456 -world ${SERVER_WORLD} -password ${SERVER_PASSWORD} -public 1 > "${STEAMAPPDIR}/server.log" &
+./valheim_server.x86_64 -name ${SERVER_NAME} -port 2456 -world ${SERVER_WORLD} -password ${SERVER_PASSWORD} -public 1 > server.log &
 
 export LD_LIBRARY_PATH=$templdpath
 
